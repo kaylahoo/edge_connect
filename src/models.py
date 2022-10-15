565941d3865d117453613c3c83bbae6ae760df53
kaylahoo <790970276@qq.com>
2022-10-15 20:43:28 +0800

    edge_connect.iml

--- src/models.py
+++ src/models.py
@@ -52,107 +52,103 @@ class BaseModel(nn.Module):
         }, self.dis_weights_path)
 
 
-class EdgeModel(BaseModel):
-    def __init__(self, config):
-        super(EdgeModel, self).__init__('EdgeModel', config)
-
-        # generator input: [grayscale(1) + edge(1) + mask(1)]
-        # discriminator input: (grayscale(1) + edge(1))
-        generator = EdgeGenerator(use_spectral_norm=True)#InpaintingModel
-        discriminator = Discriminator(in_channels=2, use_sigmoid=config.GAN_LOSS != 'hinge')
-        if len(config.GPU) > 1:
-            generator = nn.DataParallel(generator, config.GPU)
-            discriminator = nn.DataParallel(discriminator, config.GPU)
-        l1_loss = nn.L1Loss()
-        adversarial_loss = AdversarialLoss(type=config.GAN_LOSS)
-
-        self.add_module('generator', generator)
-        self.add_module('discriminator', discriminator)
-
-        self.add_module('l1_loss', l1_loss)
-        self.add_module('adversarial_loss', adversarial_loss)
-
-        self.gen_optimizer = optim.Adam(
-            params=generator.parameters(),
-            lr=float(config.LR),
-            betas=(config.BETA1, config.BETA2)
-        )
-
-        self.dis_optimizer = optim.Adam(
-            params=discriminator.parameters(),
-            lr=float(config.LR) * float(config.D2G_LR),
-            betas=(config.BETA1, config.BETA2)
-        )
-
-    def process(self, images, masks):
-        self.iteration += 1
-
-
-        # zero optimizers
-        self.gen_optimizer.zero_grad()
-        self.dis_optimizer.zero_grad()
-
-
-        # process outputs
-        outputs = self(images, masks)
-        gen_loss = 0
-        dis_loss = 0
-
-
-        # discriminator loss
-        #dis_input_real = torch.cat((images, edges), dim=1)
-        dis_input_real = images
-        dis_input_fake = torch.cat((images, outputs.detach()), dim=1)
-        dis_real, dis_real_feat = self.discriminator(dis_input_real)        # in: (grayscale(1) + edge(1))
-        dis_fake, dis_fake_feat = self.discriminator(dis_input_fake)        # in: (grayscale(1) + edge(1))
-        dis_real_loss = self.adversarial_loss(dis_real, True, True)
-        dis_fake_loss = self.adversarial_loss(dis_fake, False, True)
-        dis_loss += (dis_real_loss + dis_fake_loss) / 2
-
-
-        # generator adversarial loss
-        gen_input_fake = torch.cat((images, outputs), dim=1)
-        gen_fake, gen_fake_feat = self.discriminator(gen_input_fake)        # in: (grayscale(1) + edge(1))
-        gen_gan_loss = self.adversarial_loss(gen_fake, True, False)
-        gen_loss += gen_gan_loss
-
-
-        # generator feature matching loss
-        gen_fm_loss = 0
-        for i in range(len(dis_real_feat)):
-            gen_fm_loss += self.l1_loss(gen_fake_feat[i], dis_real_feat[i].detach())
-        gen_fm_loss = gen_fm_loss * self.config.FM_LOSS_WEIGHT
-        gen_loss += gen_fm_loss
-
-
-        # create logs
-        logs = [
-            ("l_d1", dis_loss.item()),
-            ("l_g1", gen_gan_loss.item()),
-            ("l_fm", gen_fm_loss.item()),
-        ]
-
-        return outputs, gen_loss, dis_loss, logs
-
-    def forward(self, images, masks):
-        # edges_masked = (edges * (1 - masks))
-        # images_masked = (images * (1 - masks)) + masks
-        # inputs = torch.cat((images_masked, edges_masked, masks), dim=1)
-        # outputs = self.generator(inputs)                                    # in: [grayscale(1) + edge(1) + mask(1)]
-        images_masked = images * masks.float()
-        inputs = torch.cat((images_masked, masks), dim=1)
-        outputs = self.generator(inputs)
-        return outputs
-
-    def backward(self, gen_loss=None, dis_loss=None):
-        if dis_loss is not None:
-            dis_loss.backward()
-
-        if gen_loss is not None:
-            gen_loss.backward()
-
-        self.dis_optimizer.step()
-        self.gen_optimizer.step()
+# class EdgeModel(BaseModel):
+#     def __init__(self, config):
+#         super(EdgeModel, self).__init__('EdgeModel', config)
+#
+#         # generator input: [grayscale(1) + edge(1) + mask(1)]
+#         # discriminator input: (grayscale(1) + edge(1))
+#         generator = EdgeGenerator(use_spectral_norm=True)#InpaintingModel
+#         discriminator = Discriminator(in_channels=2, use_sigmoid=config.GAN_LOSS != 'hinge')
+#         if len(config.GPU) > 1:
+#             generator = nn.DataParallel(generator, config.GPU)
+#             discriminator = nn.DataParallel(discriminator, config.GPU)
+#         l1_loss = nn.L1Loss()
+#         adversarial_loss = AdversarialLoss(type=config.GAN_LOSS)
+#
+#         self.add_module('generator', generator)
+#         self.add_module('discriminator', discriminator)
+#
+#         self.add_module('l1_loss', l1_loss)
+#         self.add_module('adversarial_loss', adversarial_loss)
+#
+#         self.gen_optimizer = optim.Adam(
+#             params=generator.parameters(),
+#             lr=float(config.LR),
+#             betas=(config.BETA1, config.BETA2)
+#         )
+#
+#         self.dis_optimizer = optim.Adam(
+#             params=discriminator.parameters(),
+#             lr=float(config.LR) * float(config.D2G_LR),
+#             betas=(config.BETA1, config.BETA2)
+#         )
+#
+#     def process(self, images, edges, masks):
+#         self.iteration += 1
+#
+#
+#         # zero optimizers
+#         self.gen_optimizer.zero_grad()
+#         self.dis_optimizer.zero_grad()
+#
+#
+#         # process outputs
+#         outputs = self(images, edges, masks)
+#         gen_loss = 0
+#         dis_loss = 0
+#
+#
+#         # discriminator loss
+#         dis_input_real = torch.cat((images, edges), dim=1)
+#         dis_input_fake = torch.cat((images, outputs.detach()), dim=1)
+#         dis_real, dis_real_feat = self.discriminator(dis_input_real)        # in: (grayscale(1) + edge(1))
+#         dis_fake, dis_fake_feat = self.discriminator(dis_input_fake)        # in: (grayscale(1) + edge(1))
+#         dis_real_loss = self.adversarial_loss(dis_real, True, True)
+#         dis_fake_loss = self.adversarial_loss(dis_fake, False, True)
+#         dis_loss += (dis_real_loss + dis_fake_loss) / 2
+#
+#
+#         # generator adversarial loss
+#         gen_input_fake = torch.cat((images, outputs), dim=1)
+#         gen_fake, gen_fake_feat = self.discriminator(gen_input_fake)        # in: (grayscale(1) + edge(1))
+#         gen_gan_loss = self.adversarial_loss(gen_fake, True, False)
+#         gen_loss += gen_gan_loss
+#
+#
+#         # generator feature matching loss
+#         gen_fm_loss = 0
+#         for i in range(len(dis_real_feat)):
+#             gen_fm_loss += self.l1_loss(gen_fake_feat[i], dis_real_feat[i].detach())
+#         gen_fm_loss = gen_fm_loss * self.config.FM_LOSS_WEIGHT
+#         gen_loss += gen_fm_loss
+#
+#
+#         # create logs
+#         logs = [
+#             ("l_d1", dis_loss.item()),
+#             ("l_g1", gen_gan_loss.item()),
+#             ("l_fm", gen_fm_loss.item()),
+#         ]
+#
+#         return outputs, gen_loss, dis_loss, logs
+#
+#     def forward(self, images, edges, masks):
+#         edges_masked = (edges * (1 - masks))
+#         images_masked = (images * (1 - masks)) + masks
+#         inputs = torch.cat((images_masked, edges_masked, masks), dim=1)
+#         outputs = self.generator(inputs)                                    # in: [grayscale(1) + edge(1) + mask(1)]
+#         return outputs
+#
+#     def backward(self, gen_loss=None, dis_loss=None):
+#         if dis_loss is not None:
+#             dis_loss.backward()
+#
+#         if gen_loss is not None:
+#             gen_loss.backward()
+#
+#         self.dis_optimizer.step()
+#         self.gen_optimizer.step()
 
 
 class InpaintingModel(BaseModel):
@@ -252,10 +248,12 @@ class InpaintingModel(BaseModel):
         return outputs, gen_loss, dis_loss, logs
 
     def forward(self, images, masks):
-        images_masked = (images * (1 - masks).float()) + masks
+        #images_masked = (images * (1 - masks).float()) + masks
+        images_masked = images * masks.float()
         #inputs = torch.cat((images_masked, edges), dim=1)
         #outputs = self.generator(inputs)                                    # in: [rgb(3) + edge(1)]
-        outputs = self.generator(images_masked)
+        inputs = torch.cat((images_masked, masks), dim=1)  # input:7
+        outputs = self.generator(inputs)
         return outputs
 
     def backward(self, gen_loss=None, dis_loss=None):
