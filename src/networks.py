d088b5c05654dcd358e82768df4b0abbc21f5911
kaylahoo <790970276@qq.com>
2022-10-16 21:57:02 +0800

    edge_connect.iml

--- src/networks.py
+++ src/networks.py
@@ -120,10 +120,10 @@ class InpaintGenerator(BaseNetwork):
             print(input_texture_mask.shape )
 
             ec_textures['ec_t_0'], ec_textures['ec_t_masks_0'] = input_image, input_texture_mask
-            print("0000"+ec_textures['ec_t_masks_0'].shape)
+            print("000", ec_textures['ec_t_masks_0'].shape)
             ec_textures['ec_t_1'], ec_textures['ec_t_masks_1'] = self.ec_texture_1(ec_textures['ec_t_0'],
                                                                                    ec_textures['ec_t_masks_0'])
-            print("1111"+ec_textures['ec_t_masks_1'].shape)
+            print("111", ec_textures['ec_t_masks_1'].shape)
             ec_textures['ec_t_2'], ec_textures['ec_t_masks_2'] = self.ec_texture_2(ec_textures['ec_t_1'],
                                                                                    ec_textures['ec_t_masks_1'])
             ec_textures['ec_t_3'], ec_textures['ec_t_masks_3'] = self.ec_texture_3(ec_textures['ec_t_2'],
