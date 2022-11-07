1b2aa16f11dec7add5170da58f7f291966162ee8
kaylahoo <790970276@qq.com>
2022-11-07 17:50:18 +0800

    edge_connect.iml

--- sample_mask.py
+++ sample_mask.py
@@ -25,6 +25,7 @@ def getRandomList(start, get_len, get_size):
 def get_mask():
     #mask_path = "../preImage"  # mask绝对路径
     mask_path = "/home/lab265/lab265/csy/datasets/irregular_mask/masks"  # mask绝对路径
+    dst_path = "/home/lab265/lab265/csy/datasets/irregular_mask"
     f = os.listdir(mask_path)
     file_list = []
     get_file_len = 100  # 每间隔n个
@@ -34,10 +35,10 @@ def get_mask():
         dir_name = str(iter_num) + '~' + str(iter_num + 10)
         if (i + 1) % get_file_len == 0:
             index = getRandomList((i + 1) - get_file_len, get_file_len, get_file_num)
-            os.makedirs(mask_path + dir_name)
+            os.makedirs(dst_path + dir_name)
             for j in index:
                 file_list.append(mask_path + f[j])
-                shutil.copy(mask_path + f[j], mask_path + dir_name + "/" + f[j])
+                shutil.copy(mask_path + f[j], dst_path + dir_name + "/" + f[j])
             iter_num = iter_num + 10
 
     print(file_list)
